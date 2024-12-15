d24d5d802d52027abbf85d425f450b9493c9ed25
zhangxuekui <303819762@qq.com>
2024-12-15 15:09:27 +0000

    print

--- src/app/documents/[documentId]/toolbar.tsx
+++ src/app/documents/[documentId]/toolbar.tsx
@@ -2,7 +2,7 @@
 
 import { cn } from "@/lib/utils";
 import { useEditorStore } from "@/store/use-editor-store";
-import { LucideIcon, PrinterIcon, Redo2Icon, Undo2Icon } from "lucide-react"
+import { LucideIcon, PrinterIcon, Redo2Icon, SpellCheckIcon, Undo2Icon } from "lucide-react"
 
 interface ToolBarButtonProps {
     onClick?: () => void;
@@ -44,6 +44,14 @@ export const ToolBar = () => {
             label: "Print",
             icon: PrinterIcon,
             onClick: () => window.print(),
+        },
+        {
+            label: "Spell Check",
+            icon: SpellCheckIcon,
+            onClick: () => {
+                const current = editor?.view.dom.getAttribute("spellcheck");
+                editor?.view.dom.setAttribute("spellcheck", current === "false" ? "true" : "false");
+            },
         }
     ]];
     return (
